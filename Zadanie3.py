dc83b8cb7148f3cd7eed3f1909a77e3c821e879c
lukedrum <32622061+lukedrum@users.noreply.github.com>
2020-05-18 11:49:55 +0200

    Create Zadanie3.py

---
+++ Zadanie3.py
@@ -0,0 +1,9 @@
+# Zadanie 3
+#
+# Wykorzystując zdobytą wiedzę, napisz skrypt symulujący skanowanie portów w zakresie 1-1000
+# używając do tego modułu threading. Sprawdź czy wykorzystanie wątków faktycznie przyspiesza działanie skryptu.
+# Nie korzystaj z modułu socket. Skrypt ma tylko symulować pracę skanera.
+# Wykorzystaj poniższy fragment kodu.
+
+print(str(worker_id) + " simulating work on a task" + str(task))
+time.sleep(1)
