7cbfa04955ef4547fa6de9490eeac693b41b1e87
lukedrum <32622061+lukedrum@users.noreply.github.com>
2020-05-18 14:01:48 +0200

    Update Zadanie3.py

--- Zadanie3.py
+++ Zadanie3.py
@@ -5,5 +5,35 @@
 # Nie korzystaj z modułu socket. Skrypt ma tylko symulować pracę skanera.
 # Wykorzystaj poniższy fragment kodu.
 
-print(str(worker_id) + " simulating work on a task" + str(task))
-time.sleep(1)
+import threading
+import queue
+import time
+
+
+def main():
+    task_q = queue.Queue()
+
+    # fill tasks queue with task
+    # Place Your code here 
+            
+    #######################
+   
+    # create worker code here
+    def worker(worker_id, task_q):
+            # Place Your code here 
+            
+            #######################
+
+            print(str(worker_id) + " simulating work on a task" + str(task))
+            time.sleep(1)
+
+        return True
+  
+    # use threads to perform worker tasks
+    # Place Your code here 
+    
+    #####################################
+
+
+if __name__ == "__main__":
+    main()
