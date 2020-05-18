bff8781e0efebe11d79356124a8e8a227af06595
lukedrum <32622061+lukedrum@users.noreply.github.com>
2020-05-18 13:26:39 +0200

    Update Zadanie2.py

--- Zadanie2.py
+++ Zadanie2.py
@@ -1,5 +1,14 @@
 # Korzystając z modułu socket i wiedzy o zapytaniach HTTP, 
 # uzyskaj informację jaki serwer i w jakiej wersji jest uruchumiony pod adresem kretes.xyz
+# Na wyjściu programu powinieneś uzyskać tylko informację o serwerze 
+#
+# Podpowiedź 1:
+# http używa \r\n. Przy podziale na nowe linie zostaje \r. Aby temu zapobiec, użyteczna może być finkcja strip()
+#
+# Podpowiedź 2:
+# Zadanie można wykonać korzystając z wyrażeń regularnych, jednak łatwiej będzie to zrobić 
+# wykorzystując funkcję startswitch() objektu string
+#
 # Wykorzystaj poniższy szkielet kodu
 
 import socket
