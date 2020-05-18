f6861b68e06d9e52ec8580ed5d8e7937eaf6fd7b
lukedrum <32622061+lukedrum@users.noreply.github.com>
2020-05-18 13:16:22 +0200

    Create Zadanie2.py

---
+++ Zadanie2.py
@@ -0,0 +1,20 @@
+# Korzystając z modułu socket i wiedzy o zapytaniach HTTP, 
+# uzyskaj informację jaki serwer i w jakiej wersji jest uruchumiony pod adresem kretes.xyz
+# Wykorzystaj poniższy szkielet kodu
+
+import socket
+
+
+def http_response():
+    target_host = "kretes.xyz"
+
+    target_port = 80
+    
+    ###########################
+    ### Miejsce na Twój kod ###
+    
+    ###########################
+
+
+if __name__ == "__main__":
+    http_response()
